847b1ab2221d0bbfb041144e4fbd3b140516f7d3
Juraj Kiss <juraj.kiss@webikon.sk>
2015-12-28 17:18:36 +0100

    Add jquery as theme.js dependency, closes #2

--- inc/assets.php
+++ inc/assets.php
@@ -5,12 +5,12 @@
 function _s_scripts() {
 	if ( WP_DEBUG ) {
 		wp_enqueue_style( '_s-style', get_template_directory_uri() . '/dist/css/style.css', array(), '0.0.1' );
-		wp_enqueue_script( '_s-theme', get_template_directory_uri() . '/dist/js/theme.js', array(), '0.0.1', true );
+		wp_enqueue_script( '_s-theme', get_template_directory_uri() . '/dist/js/theme.js', array( 'jquery' ), '0.0.1', true );
 	} else {
 		wp_enqueue_style( '_s-style', get_template_directory_uri() . '/dist/css/style.min.css', array(), '0.0.1' );
-		wp_enqueue_script( '_s-theme', get_template_directory_uri() . '/dist/js/theme.min.js', array(), '0.0.1', true );
+		wp_enqueue_script( '_s-theme', get_template_directory_uri() . '/dist/js/theme.min.js', array( 'jquery' ), '0.0.1', true );
 	}
-	
+
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
